a7d4a3b137e46c6db8fc198358128aeb21e32c58
rahats161 <rahat161@yahoo.com>
2023-06-18 14:23:29 -0500

    assertion

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,11 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected)
-    console.log("😎" + actual + "===" + expected);
+    console.log(`Assertion Passed ${actual} === ${expected}`);
   else
-    console.log("🤦" + actual + " !== " + expected);
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
 };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2,1);
-assertEqual("Winnipeg", "Winnipeg");
+assertEqual(5, 4);
